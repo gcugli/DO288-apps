dd5390a01a5e695a83299c21b2684f0aabcadc48
Student User <student@workstation.lab.example.com>
2022-09-26 11:42:42 -0400

    do288

--- go-hello/app.go
+++ go-hello/app.go
@@ -21,9 +21,9 @@ func rootHandler(response http.ResponseWriter, request *http.Request) {
 
     switch *lang {
       case "en":
-        fmt.Fprintf(response, "Hello %s!. Welcome!\n", request.URL.Path[1:])
+        fmt.Fprintf(response, "Hello %s!. Welcome do288 go!\n", request.URL.Path[1:])
       case "es":
-        fmt.Fprintf(response, "Hola %s!. Bienvenido!\n", request.URL.Path[1:])
+        fmt.Fprintf(response, "Hola %s!. Bienvenido288 go!\n", request.URL.Path[1:])
       default:
         fmt.Fprintf(response, "Error! unknown lang option -> %s\n", *lang)
   }
